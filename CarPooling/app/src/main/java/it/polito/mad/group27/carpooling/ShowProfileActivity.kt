50f3e9860a7290fe6cc99efda409457adefa22f1
e-caste-mbp <en.cas.dev@icloud.com>
2021-03-25 10:48:21 +0100

    handle edit button

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ShowProfileActivity.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ShowProfileActivity.kt
@@ -2,8 +2,10 @@ package it.polito.mad.group27.carpooling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +17,20 @@ class ShowProfileActivity : AppCompatActivity() {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.show_profile_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.edit_profile -> {
+                editProfile()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun editProfile() {
+        Log.d("MAD-group-27", "edit button clicked")
     }
 }
