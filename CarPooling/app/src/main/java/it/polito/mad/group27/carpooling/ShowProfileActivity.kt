6778ddfedf09bd53ffddf06692d4346a6314fc3e
e-caste-mbp <en.cas.dev@icloud.com>
2021-03-25 10:42:08 +0100

    add edit button to top right

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ShowProfileActivity.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ShowProfileActivity.kt
@@ -2,10 +2,18 @@ package it.polito.mad.group27.carpooling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.show_profile_menu, menu)
+        return true
     }
 }
