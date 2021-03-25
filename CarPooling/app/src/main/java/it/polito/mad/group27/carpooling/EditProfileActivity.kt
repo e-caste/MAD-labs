84c50b424a71ee5d910f66bc53561bcc6386251d
e-caste-mbp <en.cas.dev@icloud.com>
2021-03-25 11:02:33 +0100

    add edit profile activity layout

---
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/EditProfileActivity.kt
@@ -0,0 +1,11 @@
+package it.polito.mad.group27.carpooling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
