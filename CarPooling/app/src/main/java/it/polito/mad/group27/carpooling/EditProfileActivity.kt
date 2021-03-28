e5688fe66cf6763cf28f5d59c31b5503fc29ab79
MacSpowman <matteo.borzi@gmail.com>
2021-03-28 19:21:52 +0200

    src: implement activities interaction

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/EditProfileActivity.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/EditProfileActivity.kt
@@ -1,25 +1,38 @@
 package it.polito.mad.group27.carpooling
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.ImageButton
 
 class EditProfileActivity : AppCompatActivity() {
+
+    private lateinit var profile: Profile
+    private lateinit var imageButton: ImageButton
+    private lateinit var fullNameEdit : EditText
+    private lateinit var nickNameEdit : EditText
+    private lateinit var emailEdit : EditText
+    private lateinit var locationEdit : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
 
-        val profile = intent.getParcelableExtra<Profile>("group27.lab1.profile") ?: Profile()
+        profile = intent.getParcelableExtra<Profile>("group27.lab1.profile") ?: Profile()
         Log.d(getLogTag(), "received object of class ${profile::class.java}: $profile")
 
-        val imageButton = findViewById<ImageButton>(R.id.imageProfileButton)
-        val fullNameEdit = findViewById<EditText>(R.id.fullNameEdit)
-        val nickNameEdit = findViewById<EditText>(R.id.nicknameEdit)
-        val emailEdit = findViewById<EditText>(R.id.emailEdit)
-        val locationEdit = findViewById<EditText>(R.id.locationEdit)
+        imageButton = findViewById(R.id.imageProfileButton)
+        fullNameEdit = findViewById(R.id.fullNameEdit)
+        nickNameEdit = findViewById(R.id.nicknameEdit)
+        emailEdit = findViewById(R.id.emailEdit)
+        locationEdit = findViewById(R.id.locationEdit)
 
         // TODO img
         fullNameEdit.setText(profile.fullName)
@@ -30,5 +43,37 @@ class EditProfileActivity : AppCompatActivity() {
         imageButton.setOnClickListener {
             Log.d(getLogTag(), "image button clicked")
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.edit_profile_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.save_profile -> {
+                saveProfile()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun saveProfile() {
+        //TODO img
+        profile.fullName = fullNameEdit.text.toString()
+        profile.nickName = nickNameEdit.text.toString()
+        profile.email = emailEdit.text.toString()
+        profile.location = locationEdit.text.toString()
+
+        val intent = Intent().also {
+            it.putExtra("group27.lab1.profileresult", profile)
+        }
+
+        setResult(Activity.RESULT_OK, intent)
+        finish()
     }
 }
--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ShowProfileActivity.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ShowProfileActivity.kt
@@ -1,15 +1,17 @@
 package it.polito.mad.group27.carpooling
 
+import android.app.Activity
 import android.content.Intent
+import android.media.Rating
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
 
 fun AppCompatActivity.getLogTag(): String {
     return getString(R.string.log_tag)
@@ -17,7 +19,14 @@ fun AppCompatActivity.getLogTag(): String {
 
 class ShowProfileActivity : AppCompatActivity() {
 
-    private val profile = Profile()
+    private var profile = Profile()
+    private lateinit var profileImageView: ImageView
+    private lateinit var fullNameView : TextView
+    private lateinit var nickNameView : TextView
+    private lateinit var emailView : TextView
+    private lateinit var locationView : TextView
+    private lateinit var registrationDateView: TextView
+    private lateinit var reputationBar: RatingBar
 
     enum class RequestCodes {
         EDIT_PROFILE
@@ -30,6 +39,17 @@ class ShowProfileActivity : AppCompatActivity() {
         // if something in storage -> set it
         // TODO get data from storage
         // else use defaults from Profile
+
+        profileImageView = findViewById(R.id.profileImageView)
+        fullNameView = findViewById(R.id.fullNameView)
+        nickNameView = findViewById(R.id.nicknameView)
+        emailView = findViewById(R.id.emailView)
+        locationView = findViewById(R.id.locationView)
+        registrationDateView = findViewById(R.id.registrationDateView)
+        reputationBar = findViewById(R.id.ratingBar)
+
+        updateFields()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -57,5 +77,35 @@ class ShowProfileActivity : AppCompatActivity() {
 
 //        Json.decodeFromString<Profile>(Json.encodeToString(profile))
         startActivityForResult(editIntent, RequestCodes.EDIT_PROFILE.ordinal)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when(requestCode) {
+            RequestCodes.EDIT_PROFILE.ordinal -> {
+                Log.d(getLogTag(), "returned $resultCode with ${data?.getParcelableExtra<Profile>("group27.lab1.profileresult")?.toString()}")
+                if(resultCode == Activity.RESULT_OK){
+                    val newProfile = data?.getParcelableExtra<Profile>("group27.lab1.profileresult")
+                    if(profile != newProfile && newProfile != null){
+                        profile = newProfile
+                        updateFields()
+                        //TODO SharedPreferences
+
+                    }
+
+                    updateFields()
+                }
+            }
+            else -> super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun updateFields() {
+        //TODO img
+        fullNameView.text = profile.fullName
+        nickNameView.text = profile.nickName
+        emailView.text = profile.email
+        locationView.text = profile.location
+        registrationDateView.text = profile.registrationDate
+        reputationBar.rating = profile.rating
     }
 }
