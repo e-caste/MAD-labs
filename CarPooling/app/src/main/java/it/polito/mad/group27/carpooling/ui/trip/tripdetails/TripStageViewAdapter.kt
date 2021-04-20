ff2b04e90f7b8189ad597f956a651cfe2a6b1d60
Matteo Borzi <matteo.borzi@gmail.com>
2021-04-20 18:49:59 +0200

    src: add ViewAdapter for stage list (TripDetailsFragment) ref. #18

---
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripdetails/TripStageViewAdapter.kt
@@ -0,0 +1,33 @@
+package it.polito.mad.group27.carpooling.ui.trip.tripdetails
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import it.polito.mad.group27.carpooling.R
+import it.polito.mad.group27.carpooling.ui.trip.tripdetails.dummy.DummyStageContent.DummyStage
+
+class TripStageViewAdapter(
+        private val values: List<DummyStage>
+) : RecyclerView.Adapter<TripStageViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_trip_stage_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.stageTime.text = item.stageTime
+        holder.stageName.text = item.stageName
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val stageTime : TextView = view.findViewById(R.id.tripStageTime)
+        val stageName : TextView = view.findViewById(R.id.tripStageName)
+    }
+}
