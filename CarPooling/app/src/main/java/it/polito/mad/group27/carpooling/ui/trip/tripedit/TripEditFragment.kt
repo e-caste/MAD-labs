7419c890cbbd1baae9e8959e85ba6c683c11ecc5
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-04-20 20:17:45 +0200

    src: add basic DatePicker implementation

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/TripEditFragment.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/TripEditFragment.kt
@@ -2,10 +2,18 @@ package it.polito.mad.group27.carpooling.ui.trip.tripedit
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.commit
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
 import it.polito.mad.group27.carpooling.R
+import it.polito.mad.group27.carpooling.getLogTag
 import it.polito.mad.group27.carpooling.ui.trip.Date
 import it.polito.mad.group27.carpooling.ui.trip.Trip
 import java.time.LocalDateTime
@@ -24,23 +32,47 @@ class TripEditFragment : Fragment(R.layout.trip_edit_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val date = view.findViewById<EditText>(R.id.editDateText)
+        val trip = arguments?.getParcelable<Trip>("trip") ?: Trip()
 
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            date.setText(
-                arguments?.getParcelable<Trip>("trip")?.date?.toString(Locale.getDefault())
-                ?: Date(
-                    LocalDateTime.now().dayOfMonth ,
-                    LocalDateTime.now().monthValue,
-                    LocalDateTime.now().year)
-                        .toString(Locale.getDefault()))
+        val constraintsBuilder =
+            CalendarConstraints.Builder()
+                .setValidator(DateValidatorPointForward.now())
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .setCalendarConstraints(constraintsBuilder.build())
+                .build()
+        datePicker.addOnPositiveButtonClickListener {
+            Log.d(getLogTag(), "positive")
+            Log.d(getLogTag(), "${datePicker.selection.toString()}")
+
         }
-        else{
-            date.setText(
-                arguments?.getParcelable<Trip>("trip")?.date?.toString(Locale.getDefault())
-                    ?:
-                    Date(java.util.Calendar.getInstance()).toString(Locale.getDefault())
-            )
+        datePicker.addOnNegativeButtonClickListener {
+            Log.d(getLogTag(), "negative")
+        }
+        datePicker.addOnCancelListener {
+            Log.d(getLogTag(), "cancel")
+        }
+        datePicker.addOnDismissListener {
+            Log.d(getLogTag(), "dismiss")
+        }
+
+        val date = view.findViewById<TextView>(R.id.editDateText)
+        date.text = trip.date.toString(Locale.getDefault())
+        date.setOnClickListener {
+            if(!datePicker.isVisible)
+                datePicker.show(requireActivity().supportFragmentManager, "tag")
+        }
+
+        val from = PlaceHourFragment("From")
+        activity?.supportFragmentManager?.commit {
+            add(R.id.editFrom, from)
+        }
+
+        val to = PlaceHourFragment("To")
+        activity?.supportFragmentManager?.commit {
+            add(R.id.editTo, to)
         }
 
     }
