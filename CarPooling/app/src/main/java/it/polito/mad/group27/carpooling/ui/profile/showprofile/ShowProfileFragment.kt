d1bf57ac217b223ed08bb1871ae0cd740c7e0769
Roberto Bressani <roberto.bressani97@gmail.com>
2021-04-22 17:52:27 +0200

    src: show profile uses information stored in profile

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/profile/showprofile/ShowProfileFragment.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/profile/showprofile/ShowProfileFragment.kt
@@ -19,12 +19,49 @@ class ShowProfileFragment : BaseFragmentWithToolbar(
 
     private lateinit var viewModel: ShowProfileViewModel
 
+    private lateinit var profileImageView: ImageView
+    private lateinit var nickNameView: TextView
+    private lateinit var emailView: TextView
+    private lateinit var locationView: TextView
+    private lateinit var registrationDateView: TextView
+    private lateinit var reputationBar: RatingBar
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ShowProfileViewModel::class.java)
         // TODO: Use the ViewModel
 
         //TODO insert title name
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        profileImageView = view.findViewById(R.id.imageProfileView)
+        nickNameView = view.findViewById(R.id.nicknameView)
+        emailView = view.findViewById(R.id.emailView)
+        locationView = view.findViewById(R.id.locationView)
+        registrationDateView = view.findViewById(R.id.registrationDateView)
+        reputationBar = view.findViewById(R.id.ratingBar)
+
+        updateFields()
+    }
+
+    private fun updateFields() {
+        val act = activity as MainActivity
+        val profile = act.profile
+        val profileImage = act.profileImage
+
+        updateTitle(profile.fullName)
+
+        profileImageView.setImageBitmap(profileImage)
+        nickNameView.text = profile.nickName
+        emailView.text = profile.email
+        locationView.text = profile.location
+        registrationDateView.text = profile.registrationDate
+        reputationBar.rating = profile.rating
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
