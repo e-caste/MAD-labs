0f70fe511efe5c3c403fa9b26075fcb83b569bfe
Roberto Bressani <roberto.bressani97@gmail.com>
2021-04-22 17:50:37 +0200

    src: added utils function to work on profile

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/MainActivity.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/MainActivity.kt
@@ -1,6 +1,9 @@
 package it.polito.mad.group27.carpooling
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,6 +13,10 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var navView: NavigationView
     lateinit var drawerLayout: DrawerLayout
     lateinit var profile: Profile
+    lateinit var profileImage: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,10 +40,37 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(R.id.nav_trips, R.id.nav_profile ), drawerLayout)
         navView.setupWithNavController(navController)
+
+        loadProfile()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun loadProfile(){
+        // if something in storage -> set it
+        val savedProfileJson = getPreferences(MODE_PRIVATE)
+            ?.getString(getString(R.string.saved_profile_preference), null)
+        if (savedProfileJson != null) {
+            try {
+                profile = Json.decodeFromString(savedProfileJson)
+            } catch (e: SerializationException) {
+                Log.d(getLogTag(), "Cannot parse saved preference profile")
+            }
+        }else{
+            profile = Profile()
+        }
+
+
+        if (File(filesDir, getString(R.string.profile_image)).exists()) {
+            val profileImageFile = File(filesDir, getString(R.string.profile_image))
+            profileImage = BitmapFactory.decodeFile(profileImageFile.absolutePath)
+            Log.d(getLogTag(), "image is set, using user selected image...")
+        } else {
+            profileImage = BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_person_24)
+            Log.d(getLogTag(), "image is removed, setting default icon...")
+        }
     }
 }
