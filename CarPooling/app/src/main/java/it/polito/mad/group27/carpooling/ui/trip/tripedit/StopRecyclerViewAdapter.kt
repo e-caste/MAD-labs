6ff673647379ed4986b39b3d5051490bf4d7b997
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-04-24 19:11:30 +0200

    src: add hint to stops

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/StopRecyclerViewAdapter.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/StopRecyclerViewAdapter.kt
@@ -4,33 +4,31 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.timepicker.MaterialTimePicker
-import com.google.android.material.timepicker.TimeFormat
 import it.polito.mad.group27.carpooling.R
-import it.polito.mad.group27.carpooling.ui.trip.Hour
 import it.polito.mad.group27.carpooling.ui.trip.Stop
 
 
-class StopRecyclerViewAdapter(val stops: MutableList<Stop>, val context: Context): RecyclerView.Adapter<StopRecyclerViewAdapter.ItemViewHolder>(){
-    class ItemViewHolder(v: View, val context: Context): RecyclerView.ViewHolder(v){
+class StopRecyclerViewAdapter(val stops: MutableList<Stop>, val context: Context) :
+    RecyclerView.Adapter<StopRecyclerViewAdapter.ItemViewHolder>() {
+    class ItemViewHolder(v: View, val context: Context) : RecyclerView.ViewHolder(v) {
         private val placeView = v.findViewById<TextInputEditText>(R.id.stop_place)
         private val hourView = v.findViewById<TextInputEditText>(R.id.stop_hour)
-        private var timePicker : MaterialTimePicker? = null
-        //todo set hints for the various stops
+        private var timePicker: MaterialTimePicker? = null
 
-        fun bind(stop: Stop) {
+        fun bind(stop: Stop, position: Int) {
             placeView.setText(stop.place)
+            placeView.hint = "Stop ${position+1}"
             hourView.setText(stop.hour.toString())
             hourView.setOnClickListener {
-                if(timePicker == null || !timePicker?.isVisible!!) {
+                if (timePicker == null || !timePicker?.isVisible!!) {
                     timePicker = getTimePicker(
                         hourView,
                         stop.hour
-                    ){
+                    ) {
                         stop.hour.updateTime(it)
                     }
                     timePicker!!.show(
@@ -54,16 +52,16 @@ class StopRecyclerViewAdapter(val stops: MutableList<Stop>, val context: Context
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        holder.bind(stops[position])
+        holder.bind(stops[position], position)
     }
 
-    fun add(stop: Stop){
+    fun add(stop: Stop) {
         stops.add(stop)
         this.notifyItemInserted(stops.size - 1)
     }
 
-    fun remove(){
-        stops.removeAt(stops.size-1)
+    fun remove() {
+        stops.removeAt(stops.size - 1)
         this.notifyItemRemoved(stops.size)
     }
 
