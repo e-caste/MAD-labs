8c5ab52a5ab77ac6355334abeeccb94838b63bd6
Roberto Bressani <roberto.bressani97@gmail.com>
2021-04-24 18:07:04 +0200

    fix: added missing function to load profile image when no image is present

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/utils.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/utils.kt
@@ -1,9 +1,15 @@
 package it.polito.mad.group27.carpooling
 
+import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import java.io.OutputStream
+
 
 fun Fragment.getLogTag(): String {
     return getString(R.string.log_tag)
@@ -22,4 +28,19 @@ fun OutputStream.writeBitmap(
         bitmap.compress(format, quality, out)
         out.flush()
     }
+}
+
+fun getBitmapFromVectorDrawable(context: Context, drawableId: Int): Bitmap? {
+    var drawable = ContextCompat.getDrawable(context, drawableId)
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+        drawable = DrawableCompat.wrap(drawable!!).mutate()
+    }
+    val bitmap = Bitmap.createBitmap(
+        drawable!!.intrinsicWidth,
+        drawable.intrinsicHeight, Bitmap.Config.ARGB_8888
+    )
+    val canvas = Canvas(bitmap)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+    return bitmap
 }
