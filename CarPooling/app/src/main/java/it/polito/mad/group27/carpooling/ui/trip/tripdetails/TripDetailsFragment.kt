37762fab119eaa8a5cd3b718c827556c989b3cd6
Matteo Borzi <matteo.borzi@gmail.com>
2021-04-27 16:06:42 +0200

    src: trip details basic info display through bundle

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripdetails/TripDetailsFragment.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripdetails/TripDetailsFragment.kt
@@ -6,22 +6,29 @@ import android.util.Log
 import android.view.*
 import android.widget.ImageView
 import android.widget.LinearLayout
-import androidx.appcompat.widget.Toolbar
+import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import it.polito.mad.group27.carpooling.MainActivity
 import it.polito.mad.group27.carpooling.R
 import it.polito.mad.group27.carpooling.getLogTag
 import it.polito.mad.group27.carpooling.ui.BaseFragmentWithToolbar
+import it.polito.mad.group27.carpooling.ui.trip.Trip
 import it.polito.mad.group27.carpooling.ui.trip.tripdetails.dummy.DummyStageContent
+import java.text.DateFormat
+import java.util.*
 
 class TripDetailsFragment : BaseFragmentWithToolbar(R.layout.trip_details_fragment,
     R.menu.show_menu, null) {
     // TODO insert title customized (Trip to .... ) (?)
     private lateinit var viewModel: TripDetailsViewModel
     private lateinit var dropdownListButton : LinearLayout
+
+    private lateinit var trip : Trip
+    private lateinit var seatsView : TextView
+    private lateinit var dateView : TextView
+    private lateinit var priceView : TextView
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -53,8 +60,17 @@ class TripDetailsFragment : BaseFragmentWithToolbar(R.layout.trip_details_fragme
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        trip = arguments?.getParcelable<Trip>("trip") ?: Trip()
+
         dropdownListButton = view.findViewById(R.id.startTripView)
 
+        seatsView = view.findViewById(R.id.showTripSeats)
+        dateView = view.findViewById(R.id.showTripDate)
+        priceView = view.findViewById(R.id.showTripPrice)
+
+        seatsView.text = "${trip.availableSeats}/5"
+        dateView.text =  DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault()).format(trip.date)
+        priceView.text = trip.price.toString()
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.tripStageList)
         recyclerView.layoutManager = LinearLayoutManager(context)
