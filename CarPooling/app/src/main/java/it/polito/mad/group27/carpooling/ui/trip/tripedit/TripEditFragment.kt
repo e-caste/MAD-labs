187b293636816d2f3adb3e86f8a317d9edf923ef
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-04-28 18:01:27 +0200

    fix: estimated time was not updated on date changed

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/TripEditFragment.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/TripEditFragment.kt
@@ -116,10 +116,12 @@ class TripEditFragment : EditFragment(R.layout.trip_edit_fragment,
                     to_hour.error = getString(R.string.edit_to_hour_error)
                     to_date.error = null
                 }
+                setEstimatedTime()
             },
             {
                 from_date.error = null
                 to_hour.error = null
+                setEstimatedTime()
             }
         )
 
@@ -290,7 +292,6 @@ class TripEditFragment : EditFragment(R.layout.trip_edit_fragment,
         this.set(Calendar.HOUR_OF_DAY, timePicker.hour)
         this.set(Calendar.MINUTE, timePicker.minute)
         Log.d(getLogTag(), "update time to : ${timePicker.hour} : ${timePicker.minute}")
-        setEstimatedTime()
         return this
     }
 
