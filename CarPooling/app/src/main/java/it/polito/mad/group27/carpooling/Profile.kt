e52f0a7041487b55573db318cd5ed39036e88759
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-04-29 13:11:23 +0200

    src: update profle class

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/Profile.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/Profile.kt
@@ -1,16 +1,25 @@
 package it.polito.mad.group27.carpooling
 
+import android.net.Uri
 import android.os.Parcelable
+import it.polito.mad.group27.carpooling.ui.trip.CalendarSerializer
+import it.polito.mad.group27.carpooling.ui.trip.UriSerializer
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
+import java.util.*
 
 @Serializable
 @Parcelize
 data class Profile (
+    val uid: String,
+    @Serializable(with= UriSerializer::class)
+    var profileImageUri: Uri? = null,
     var fullName: String = "John Smith",
     var nickName: String = "MadJohn",
     var email: String = "john.smith@polito.it",
     var location: String = "Turin, Italy",
-    var registrationDate: String = "25/03/2021",
-    var rating: Float = 4.5f,
+    @Serializable(with= CalendarSerializer::class)
+    var registrationDate: Calendar = Calendar.getInstance(),
+    var rating: Float = 4.5f
+    // TODO implement rating logic
 ): Parcelable
