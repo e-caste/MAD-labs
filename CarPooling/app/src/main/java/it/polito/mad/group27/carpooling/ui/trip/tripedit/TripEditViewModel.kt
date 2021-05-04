2c2f11faab74517b9cf55a6fc1c90306c2730b12
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-05-04 15:30:37 +0200

    src: implement functions to deal with editTrip data

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/TripEditViewModel.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/TripEditViewModel.kt
@@ -15,6 +15,46 @@ class TripEditViewModel(private val context: Context) : ViewModel() {
     lateinit var trip: Trip
     lateinit var newTrip : Trip
 
-class TripEditViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private lateinit var userProfiles : Map<String, Profile>
+    private val db = FirebaseFirestore.getInstance()
+    private val users = db.collection("users")
+
+    fun downloadUsers(callback : ( Map<String, Profile> )-> Unit){
+        var mapTmp : MutableMap<String, Profile> = mutableMapOf()
+        for (uid in newTrip.acceptedUsersUids){
+            mapTmp[uid] = downloadProfileByUid(uid)
+        }
+        for (uid in newTrip.interestedUsersUids){
+            mapTmp[uid] = downloadProfileByUid(uid)
+        }
+        userProfiles = mapTmp.toMap()
+        callback(userProfiles)
+    }
+
+    private fun downloadProfileByUid(uid:String) : Profile{
+        return users.document(uid).get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d(context.getString(R.string.log_tag), "DocumentSnapshot data: ${document.data}")
+                    document as Profile
+                } else {
+                    Log.d(context.getString(R.string.log_tag), "No such document")
+                    Profile(nickName = "UNAVAILABLE")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(context.getString(R.string.log_tag), "get failed with ", exception)
+                Profile(nickName = "UNAVAILABLE")
+            } as Profile
+    }
+
+    fun getProfileByUid(uid:String): Profile{
+        return userProfiles[uid] ?: Profile(nickName = "UNAVAILABLE")
+    }
+
+    fun putToAccepted(uid: String){
+        newTrip.interestedUsersUids.remove(uid)
+        newTrip.acceptedUsersUids.add(uid)
+    }
+
 }
