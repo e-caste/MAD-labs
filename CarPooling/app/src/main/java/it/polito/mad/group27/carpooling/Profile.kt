f12c3e6babaa995766316ed711f1d589872936a5
Roberto Bressani <roberto.bressani97@gmail.com>
2021-05-04 11:23:10 +0200

    src: updated profile to persist it on the db

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/Profile.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/Profile.kt
@@ -1,25 +1,22 @@
 package it.polito.mad.group27.carpooling
 
-import android.net.Uri
 import android.os.Parcelable
-import it.polito.mad.group27.carpooling.ui.trip.CalendarSerializer
-import it.polito.mad.group27.carpooling.ui.trip.UriSerializer
+import com.google.firebase.Timestamp
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
-import java.util.*
 
 @Serializable
 @Parcelize
-data class Profile (
-    val uid: String,
-    @Serializable(with= UriSerializer::class)
-    var profileImageUri: Uri? = null,
+data class Profile(
+    val uid: String?=null,
+    var profileImageUri: String? = null,
     var fullName: String = "John Smith",
     var nickName: String = "MadJohn",
     var email: String = "john.smith@polito.it",
     var location: String = "Turin, Italy",
-    @Serializable(with= CalendarSerializer::class)
-    var registrationDate: Calendar = Calendar.getInstance(),
+    @Contextual
+    var registrationDate: Timestamp = Timestamp.now(),
     var rating: Float = 4.5f
     // TODO implement rating logic
-): Parcelable
+):Parcelable
