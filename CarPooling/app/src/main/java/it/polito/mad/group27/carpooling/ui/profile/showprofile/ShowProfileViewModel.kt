8f31c9b3407cea414bd277690c20bcfcd439bcec
Roberto Bressani <roberto.bressani97@gmail.com>
2021-05-04 12:48:10 +0200

    src: removed unused viewModels

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/profile/showprofile/ShowProfileViewModel.kt
+++
@@ -1,7 +0,0 @@
-package it.polito.mad.group27.carpooling.ui.profile.showprofile
-
-import androidx.lifecycle.ViewModel
-
-class ShowProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
