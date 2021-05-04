f5422d2991ea39b0979ca42f6266f5ff55f1819b
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-05-04 15:27:22 +0200

    src: move trip and newTrip to TripEditViewModel

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/TripEditViewModel.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/TripEditViewModel.kt
@@ -1,6 +1,19 @@
 package it.polito.mad.group27.carpooling.ui.trip.tripedit
 
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import it.polito.mad.group27.carpooling.Profile
+import it.polito.mad.group27.carpooling.R
+import it.polito.mad.group27.carpooling.ui.trip.Trip
+
+class TripEditViewModel(private val context: Context) : ViewModel() {
+
+    lateinit var trip: Trip
+    lateinit var newTrip : Trip
 
 class TripEditViewModel : ViewModel() {
     // TODO: Implement the ViewModel
