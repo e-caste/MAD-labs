9798cac71d152d1aed27fb964e0b679e0b259f66
Roberto Bressani <roberto.bressani97@gmail.com>
2021-05-06 16:17:28 +0200

    src: renamed fragment due to name clash

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripfilter/TripFilterFragment.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripfilter/TripFilterFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import it.polito.mad.group27.carpooling.R
 import it.polito.mad.group27.carpooling.ui.BaseFragmentWithToolbar
+import java.util.logging.Filter
 
 class TripFilter : BaseFragmentWithToolbar(R.layout.trip_filter_fragment, R.menu.edit_menu, R.string.trip_edit_title) {
 
@@ -15,11 +16,12 @@ class TripFilter : BaseFragmentWithToolbar(R.layout.trip_filter_fragment, R.menu
     private lateinit var viewModel: TripFilterViewModel
 
 
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(TripFilterViewModel::class.java)
         // TODO: Use the ViewModel
+        
+        viewModel.tripFilter = arguments?.getParcelable("filter") ?: TripFilter()
     }
 
 }
