02bd2f7592dde9740fad60cfe6854165ef1637ac
Roberto Bressani <roberto.bressani97@gmail.com>
2021-05-07 12:49:05 +0200

    src: implemented class TripDB and StopDB and converter methods

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/Trip.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/Trip.kt
@@ -4,8 +4,13 @@ import android.net.Uri
 import android.os.Build
 import android.os.Parcelable
 import androidx.annotation.RequiresApi
+import com.google.firebase.Timestamp
+import it.polito.mad.group27.carpooling.CalendarToTimestamp
+import it.polito.mad.group27.carpooling.TimestampToCalendar
 import kotlinx.parcelize.Parcelize
-import kotlinx.serialization.*
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
@@ -19,29 +24,31 @@ import java.time.LocalTime
 import java.util.*
 
 @Serializer(forClass = Date::class)
-object CalendarSerializer: KSerializer<Calendar> {
+object CalendarSerializer : KSerializer<Calendar> {
     private val df: DateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm")
 
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Date", PrimitiveKind.STRING)
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("Date", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: Calendar) {
         encoder.encodeString(df.format(value.time))
     }
 
     override fun deserialize(decoder: Decoder): Calendar {
-        return Calendar.getInstance().also{
+        return Calendar.getInstance().also {
             it.time = df.parse(decoder.decodeString())!!
         }
     }
 }
 
 @Serializer(forClass = Uri::class)
-object UriSerializer: KSerializer<Uri> {
+object UriSerializer : KSerializer<Uri> {
 
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Uri", PrimitiveKind.STRING)
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("Uri", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: Uri) {
-        encoder.encodeString(value.path ?:"")
+        encoder.encodeString(value.path ?: "")
     }
 
     override fun deserialize(decoder: Decoder): Uri {
@@ -50,11 +57,12 @@ object UriSerializer: KSerializer<Uri> {
 }
 
 @Serializer(forClass = BigDecimal::class)
-object BigDecimalSerializer: KSerializer<BigDecimal> {
+object BigDecimalSerializer : KSerializer<BigDecimal> {
 
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("BigDecimal", PrimitiveKind.STRING)
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("BigDecimal", PrimitiveKind.STRING)
 
-    override fun serialize(encoder: Encoder, value:BigDecimal) {
+    override fun serialize(encoder: Encoder, value: BigDecimal) {
         encoder.encodeString(value.toString())
     }
 
@@ -64,8 +72,6 @@ object BigDecimalSerializer: KSerializer<BigDecimal> {
 }
 
 
-
-
 @Serializable
 @Parcelize
 data class Trip(
@@ -73,17 +79,18 @@ data class Trip(
     var id: Int = -1,  // TODO: this should be a String if we want to use the Firebase ids
     var ownerUid: String = "testUid",
     // other fields
-    @Serializable(with=UriSerializer::class)
+    @Serializable(with = UriSerializer::class)
     var carImageUri: Uri? = null,
     var totalSeats: Int? = null,
     @Deprecated("Will no longer be used, use the list size")
     var availableSeats: Int? = null,
-    @Serializable(with=BigDecimalSerializer::class)
+    @Serializable(with = BigDecimalSerializer::class)
     var price: BigDecimal? = null,
-    @Serializable(with=CalendarSerializer::class)
+    @Serializable(with = CalendarSerializer::class)
     var startDateTime: Calendar = Calendar.getInstance(),
-    @Serializable(with=CalendarSerializer::class)
-    var endDateTime: Calendar = (Calendar.getInstance().clone() as Calendar).also { it.add(Calendar.HOUR, +1) },
+    @Serializable(with = CalendarSerializer::class)
+    var endDateTime: Calendar = (Calendar.getInstance()
+        .clone() as Calendar).also { it.add(Calendar.HOUR, +1) },
     var from: String = "",
     var to: String = "",
     val stops: MutableList<Stop> = mutableListOf(),
@@ -91,24 +98,105 @@ data class Trip(
     var otherInformation: String? = null,
     val acceptedUsersUids: MutableList<String> = mutableListOf(),
     val interestedUsersUids: MutableList<String> = mutableListOf(),
-): Parcelable
+) : Parcelable {
+    fun toTripDB():TripDB{
+        return TripDB(
+            id = "fakeID", //TODO ADAPT
+            ownerUid = ownerUid,
+            carImageUri = carImageUri?.toString(),
+            totalSeats = totalSeats!!,
+            price = price!!.also { it.setScale(2) }.toString(),
+            startDateTime = CalendarToTimestamp(startDateTime),
+            endDateTime = CalendarToTimestamp(endDateTime),
+            from = from,
+            to = to,
+            stops = stops.map { it.toStopDB() }.toMutableList(),
+            options = options,
+            acceptedUsersUids = acceptedUsersUids,
+            interestedUsersUids = interestedUsersUids,
+            otherInformation = otherInformation
+        )
+    }
+}
 
 @Serializable
 @Parcelize
-data class Hour(var hour: Int, var minute: Int): Parcelable{
-    constructor(calendar: Calendar) : this(calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE))
+data class Hour(var hour: Int, var minute: Int) : Parcelable {
+    constructor(calendar: Calendar) : this(
+        calendar.get(Calendar.HOUR_OF_DAY),
+        calendar.get(Calendar.MINUTE)
+    )
+
     @RequiresApi(Build.VERSION_CODES.O)
-    constructor(dateTime: LocalTime) : this(dateTime.hour , dateTime.minute)
-    override fun toString() = "${if (hour < 10) "0" else ""}$hour:${if (minute < 10) "0" else ""}$minute"
+    constructor(dateTime: LocalTime) : this(dateTime.hour, dateTime.minute)
+
+    override fun toString() =
+        "${if (hour < 10) "0" else ""}$hour:${if (minute < 10) "0" else ""}$minute"
 
 }
 
 @Serializable
 @Parcelize
-data class Stop(var place: String, @Serializable(with=CalendarSerializer::class) var dateTime: Calendar): Parcelable
+data class Stop(
+    var place: String,
+    @Serializable(with = CalendarSerializer::class) var dateTime: Calendar
+) : Parcelable {
+    fun toStopDB():StopDB{
+        return StopDB(place, CalendarToTimestamp(dateTime))
+    }
+}
 
 enum class Option {
     ANIMALS,
     LUGGAGE,
     SMOKE,
 }
+
+
+@Parcelize
+data class TripDB(
+    // primary keys
+    var id: String="",  // TODO: this should be a String if we want to use the Firebase ids
+    var ownerUid: String="",
+    var carImageUri: String?=null,
+    var totalSeats: Int=0,
+    var price: String="0.00",
+    var startDateTime: Timestamp=Timestamp.now(),
+    var endDateTime: Timestamp=Timestamp.now(),
+    var from: String="",
+    var to: String="",
+    val stops: MutableList<StopDB> = mutableListOf(),
+    val options: MutableList<Option> = mutableListOf(),
+    var otherInformation: String?=null,
+    val acceptedUsersUids: MutableList<String> = mutableListOf(),
+    val interestedUsersUids: MutableList<String> = mutableListOf()
+) : Parcelable {
+    fun toTrip(): Trip {
+        return Trip(
+            id = -1, //TODO ADAPT
+            ownerUid = ownerUid,
+            carImageUri = if (carImageUri == null) null else Uri.parse(carImageUri),
+            totalSeats = totalSeats,
+            price = BigDecimal(price).also{it.setScale(2)},
+            startDateTime = TimestampToCalendar(startDateTime),
+            endDateTime = TimestampToCalendar(endDateTime),
+            from = from,
+            to = to,
+            stops = stops.map { it.toStop() }.toMutableList(),
+            options = options,
+            acceptedUsersUids = acceptedUsersUids,
+            interestedUsersUids = interestedUsersUids,
+            otherInformation = otherInformation
+        )
+    }
+}
+
+@Parcelize
+data class StopDB(var place: String="", var dateTime: Timestamp=Timestamp.now()) : Parcelable {
+    fun toStop(): Stop {
+        return Stop(place, dateTime = TimestampToCalendar(dateTime))
+    }
+}
+
+
+
