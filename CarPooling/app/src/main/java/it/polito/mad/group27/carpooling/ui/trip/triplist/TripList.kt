1b3f959ce89911d1f26a21459faba0edd919ccef
e-caste-mbp <en.cas.dev@icloud.com>
2021-05-07 10:35:02 +0200

    implement draft of recycler view - Firestore communication. ref #27

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/triplist/TripList.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/triplist/TripList.kt
@@ -11,9 +11,14 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import it.polito.mad.group27.carpooling.R
 import it.polito.mad.group27.carpooling.createSampleDataIfNotPresent
 import it.polito.mad.group27.carpooling.getLogTag
@@ -22,6 +27,11 @@ import it.polito.mad.group27.carpooling.ui.trip.Trip
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import java.math.BigDecimal
+import java.text.DateFormat
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 /**
@@ -32,6 +42,41 @@ class TripList: BaseFragmentWithToolbar(
     R.menu.trip_list_menu,
     R.string.app_name
 ){
+
+    private val db = FirebaseFirestore.getInstance()
+    private val queryBase = db.collection(coll)
+        .whereGreaterThan("startDateTime", Calendar.getInstance())  // TODO: check if this works
+        .orderBy("startDateTime", Query.Direction.ASCENDING)
+    private val options = FirestoreRecyclerOptions.Builder<Trip>()
+        .setQuery(queryBase, Trip::class.java)
+        .build()
+    private var adapter: TripFirestoreRecyclerAdapter? = null
+
+    companion object {
+        private const val coll = "trips"
+    }
+
+    private inner class TripViewHolder internal constructor(private val view: View) : RecyclerView.ViewHolder(view) {
+        private val dateFormat: DateFormat = SimpleDateFormat("dd/MM/yyyy")
+        private val priceFormat: NumberFormat = NumberFormat.getCurrencyInstance(Locale.ITALY)
+
+        internal fun setPrice(price: BigDecimal?) {
+            val textView = view.findViewById<TextView>(R.id.price_text)
+            textView.text = priceFormat.format(price)
+        }
+        // TODO: add all other fields, carefully respect names to allow automatic (de)serialization
+    }
+
+    private inner class TripFirestoreRecyclerAdapter internal constructor(options: FirestoreRecyclerOptions<Trip>) : FirestoreRecyclerAdapter<Trip, TripViewHolder>(options) {
+        override fun onBindViewHolder(tripViewHolder: TripViewHolder, position: Int, trip: Trip) {
+            tripViewHolder.setPrice(trip.price)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TripViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_trip, parent, false)
+            return TripViewHolder(view)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,19 +105,14 @@ class TripList: BaseFragmentWithToolbar(
                 LinearLayoutManager(context)
             }
         }
-        recyclerView.adapter = TripCardRecyclerViewAdapter(trips, findNavController())
+        adapter = TripFirestoreRecyclerAdapter(options)
+        recyclerView.adapter = adapter
 
         val fab: FloatingActionButton = view.findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-            findNavController().navigate(R.id.action_tripList_to_tripEditFragment)
-        }
-
-
+        fab.setOnClickListener { findNavController().navigate(R.id.action_tripList_to_tripEditFragment) }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-
+        // TODO: do we still need to save and restore the instance state?
     }
 }
