70e432fe38fae0cf5fc421eaffe9e52723e96219
e-caste-mbp <en.cas.dev@icloud.com>
2021-05-10 11:45:52 +0200

    show either add button or checkmark on trip card based on user's trip booking status. ref #27

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/triplist/OthersTripList.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/triplist/OthersTripList.kt
@@ -1,10 +1,13 @@
 package it.polito.mad.group27.carpooling.ui.trip.triplist
 
 import android.os.Bundle
+import android.widget.Toast
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import it.polito.mad.group27.carpooling.R
 import it.polito.mad.group27.carpooling.ui.trip.Trip
 import it.polito.mad.group27.carpooling.ui.trip.TripDB
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 class OthersTripList: BaseTripList() {
 
@@ -14,12 +17,21 @@ class OthersTripList: BaseTripList() {
         .build()
 
     override fun setTopRightButtonIconAndOnClickListener(tripViewHolder: BaseTripList.TripViewHolder, bundle: Bundle) {
-        val icon = R.drawable.ic_baseline_add_24
+        val trip = Json.decodeFromString<Trip>(requireArguments().getString("trip")!!)
+        var icon: Int? = null
+        if (trip.interestedUsersUids.contains(currentUserUid) || trip.acceptedUsersUids.contains(currentUserUid)) {
+            icon = R.drawable.ic_baseline_done_24
+            tripViewHolder.topRightButton.setOnClickListener {
+                Toast.makeText(requireContext(), "You have already booked this trip!", Toast.LENGTH_LONG).show()
+            }
+        } else {
+            icon = R.drawable.ic_baseline_add_24
+            tripViewHolder.topRightButton.setOnClickListener {
+                // TODO: call Firestore API to add current user to interestedUsers and set icon to done check
+            }
+        }
         tripViewHolder.topRightButtonShadow.setImageResource(icon)
         tripViewHolder.topRightButton.setImageResource(icon)
-        tripViewHolder.topRightButton.setOnClickListener {
-            // TODO: call Firestore API to add current user to interestedUsers and set icon to done check
-        }
     }
 
     override fun filterOutTrip(trip: Trip): Boolean {
