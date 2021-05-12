b89ec81cc873dadc58abb61235f9b944d819b06e
e-caste-mbp <en.cas.dev@icloud.com>
2021-05-12 19:41:46 +0200

    only override menu in OthersTripList. ref #27

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/triplist/OthersTripList.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/triplist/OthersTripList.kt
@@ -19,11 +19,9 @@ import it.polito.mad.group27.carpooling.ui.trip.Trip
 import it.polito.mad.group27.carpooling.ui.trip.TripDB
 
 class OthersTripList(
-    layout: Int = R.layout.fragment_others_trip_list,
     menu: Int = R.menu.others_trip_list_menu,
 ): BaseTripList(
-    layout,
-    menu,
+    menu = menu,
 ) {
 
     private val query = queryBase  // .whereNotEqualTo("ownerUid", currentUserUid)  // 2 inequalities on 2 fields in 1 query are invalid. wut.
