daf58b27f8395e473f45ec3c4197a7f47ce3dba6
Matteo Borzi <matteo.borzi@gmail.com>
2021-05-19 11:28:13 +0200

    src: implement ViewAdapter for interested and accepted users ref.#30

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripdetails/TripUserDetailsViewAdapter.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripdetails/TripUserDetailsViewAdapter.kt
@@ -1,14 +1,19 @@
 package it.polito.mad.group27.carpooling.ui.trip.tripdetails
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import it.polito.mad.group27.carpooling.Profile
 import it.polito.mad.group27.carpooling.R
 
 class TripUserDetailsViewAdapter (
-    private val values: List<String>
+    private val values: List<Profile>,
+    private val context: Context
 ) : RecyclerView.Adapter<TripUserDetailsViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -20,13 +25,16 @@ class TripUserDetailsViewAdapter (
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
 
-        holder.nickname.text = item
+        if(item.profileImageUri != null)
+            Glide.with(context).load(item.profileImageUri).circleCrop().into(holder.image)
+
+        holder.nickname.text = item.nickName
     }
 
     override fun getItemCount(): Int = values.size
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val nickname: TextView = view.findViewById(R.id.nicknameView)
-        val linkButton : TextView = view.findViewById(R.id.viewProfileLink)
+        val nickname: TextView = view.findViewById(R.id.nickname_details)
+        val image: ImageView = view.findViewById(R.id.passenger_image_details)
     }
 }
