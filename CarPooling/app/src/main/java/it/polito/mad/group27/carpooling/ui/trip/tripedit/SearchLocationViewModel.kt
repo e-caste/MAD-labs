d90ffee6957748d28d38cddf913ed6b855518979
Roberto Bressani <roberto.bressani97@gmail.com>
2021-05-26 10:50:46 +0200

    src: added searchApi

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/SearchLocationViewModel.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/SearchLocationViewModel.kt
@@ -1,13 +1,29 @@
 package it.polito.mad.group27.carpooling.ui.trip.tripedit
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import it.polito.mad.group27.carpooling.getLogTag
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import org.osmdroid.util.GeoPoint
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 class SearchLocationViewModel : ViewModel() {
     val geoPoint: MutableLiveData<GeoPoint?> = MutableLiveData(null)
     val locationString: MutableLiveData<String?> = MutableLiveData(null)
     val searchSuggestions: MutableLiveData<List<Pair<String, GeoPoint>>?> = MutableLiveData(null)
+}
+
+interface SearchAPI{
+    @GET("/search")
+    suspend fun getSuggestions(@Query("q") search:String,
+                       @Query("limit") limit:Int =10,
+                       @Query("format") format:String = "json"): List<Suggestion>
 }
 data class Suggestion (
     val lat:String,
