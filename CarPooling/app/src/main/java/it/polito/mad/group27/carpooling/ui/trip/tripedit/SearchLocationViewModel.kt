7493c431179379198cec1194b1a0a25ae529f7c2
Roberto Bressani <roberto.bressani97@gmail.com>
2021-05-26 10:50:13 +0200

    src: added classes that maps return types for osm REST API

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/SearchLocationViewModel.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/SearchLocationViewModel.kt
@@ -9,3 +9,28 @@ class SearchLocationViewModel : ViewModel() {
     val locationString: MutableLiveData<String?> = MutableLiveData(null)
     val searchSuggestions: MutableLiveData<List<Pair<String, GeoPoint>>?> = MutableLiveData(null)
 }
+data class Suggestion (
+    val lat:String,
+    val lon:String,
+    val display_name:String,
+    val address:Address?
+        ){
+    val geopoint:GeoPoint
+        get()= GeoPoint(lat.toDouble(), lon.toDouble())
+
+    override fun toString():String{
+        //TODO see if addess is present and format it
+        return display_name
+    }
+}
+
+data class Address (
+    val road:String,
+    val village: String,
+    val county:String,
+    val state:String,
+    val country:String,
+    val country_code: String
+        )
+
+
