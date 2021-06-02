bd756fa3511381d88f330c20faf8ba9e11b3349c
e-caste-mbp <e@caste.dev>
2021-06-02 11:11:53 +0200

    refactor Rating: userId -> passengerUid. ref #44

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/entities/Rating.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/entities/Rating.kt
@@ -3,8 +3,8 @@ package it.polito.mad.group27.carpooling.entities
 import com.google.firebase.firestore.DocumentReference
 
 data class Rating(
-    val tripId: DocumentReference,
-    val userId: DocumentReference,
+    val tripId: DocumentReference,  // implicitly contains driverUid
+    val passengerUid: DocumentReference,
     val rating: Int,
-    val comment: String
+    val comment: String,
 )
