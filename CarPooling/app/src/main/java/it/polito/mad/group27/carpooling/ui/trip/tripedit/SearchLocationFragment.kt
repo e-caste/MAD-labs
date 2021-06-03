f1a5acd50e4651fd6618516965ed13cd37e0a77a
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-06-03 19:03:41 +0200

    src: add strings

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/SearchLocationFragment.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/ui/trip/tripedit/SearchLocationFragment.kt
@@ -258,7 +258,7 @@ class SearchLocationFragment : BaseFragmentWithToolbar(R.layout.search_location_
                 textView.visibility = View.VISIBLE
                 view.findViewById<ProgressBar>(R.id.progressBar).visibility = View.GONE
                 if(suggestions.isEmpty()){
-                    textView.text = "No results for current search"
+                    textView.text = getString(R.string.no_search_results)
                 }else {
 
                     textView.text = suggestions[position].first
