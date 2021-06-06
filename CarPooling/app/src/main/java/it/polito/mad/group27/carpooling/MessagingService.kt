b18767f02d8958466976365be4ca29127dfbbccb
Matteo Borzi <matteo.borzi@gmail.com>
2021-06-06 22:31:27 +0200

    src: MessagingService validation on return

--- CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/MessagingService.kt
+++ CarPooling/app/src/main/java/it/polito/mad/group27/carpooling/MessagingService.kt
@@ -2,33 +2,22 @@ package it.polito.mad.group27.carpooling
 
 import android.os.Parcelable
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.gson.Gson
-import it.polito.mad.group27.carpooling.ui.trip.tripedit.SearchAPI
-import it.polito.mad.group27.carpooling.ui.trip.tripedit.Suggestion
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
-import java.io.BufferedOutputStream
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
+
 
 class MessagingService : FirebaseMessagingService() {
 
     companion object Oauth {
         lateinit var oauthToken: String
 
-        const val projectId = 378735075553
+        private const val projectId = 378735075553
         const val host = "fcm.googleapis.com"
         const val path = "/v1/projects/$projectId/messages:send"
 
@@ -43,38 +32,34 @@ class MessagingService : FirebaseMessagingService() {
         }
 
 
-        fun sendNotification(userToken: String?, notification: AndroidNotification){
+        suspend fun sendNotification(userToken: String?, notification: AndroidNotification) : Boolean{
 
             if(userToken == null) {
-                //TODO notify no auth token
-                return
+                return false
             }
 
-            MainScope().launch{
+            val headers = HashMap<String, String>()
+            headers["Authorization"] = "Bearer $oauthToken"
+            headers["Host"] = host
 
-                val headers = HashMap<String, String>()
-                headers.put("Authorization", "Bearer $oauthToken")
-                headers.put("Host", host)
+            val notificationObject = NotificationObject(userToken, notification)
+            val body = Message(notificationObject, false)
 
-                val notificationObject = NotificationObject(userToken, notification)
-                val body = Message(notificationObject, false)
-
-                val response = retrofit.sendNotification(headers, body)
-                var snackText :String = when {
-                    response.isSuccessful -> {
-                        "Notification sent correctly"
-                    }
-                    response.code() == 401 -> {
-                        "Your token has expired, please reopen the app"
-                    }
-                    else -> "Generic error: ${response.body()}"
+            val response = retrofit.sendNotification(headers, body)
+            val snackText :String = when {
+                response.isSuccessful -> {
+                    "Notification sent correctly"
                 }
-
-                //TODO make snackbar properly and format response body
-                response.body()?.let { Log.d(getLogTag(), it.toString()) }
-                Log.d(getLogTag(), snackText)
+                response.code() == 401 -> {
+                    "Your token has expired, please reopen the app"
+                }
+                else -> "Generic error: ${response.body()}"
             }
 
+            response.body()?.let { Log.d(getLogTag(), it.toString()) }
+            Log.d(getLogTag(), snackText)
+
+            return response.isSuccessful
         }
     }
 
